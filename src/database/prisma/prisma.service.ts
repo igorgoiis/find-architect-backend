de7916413dc0047aa7df218304e925f75ccd9783
Igor Gois <53226826+igorgoiis@users.noreply.github.com>
2023-01-07 00:05:36 -0300

    :sparkles:  feat: add middleware in prisma service for hash of password

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -5,6 +5,7 @@ import {
   OnModuleInit,
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class PrismaService
@@ -17,6 +18,17 @@ export class PrismaService
 
   async onModuleInit() {
     await this.$connect();
+    this.$use(async (params, next) => {
+      if (params.action == 'create' && params.model == 'User') {
+        const user = params.args.data;
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(user.password, salt);
+        user.password = hash;
+        params.args.data = user;
+      }
+
+      return next(params);
+    });
   }
 
   async onModuleDestroy() {
